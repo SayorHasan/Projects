3d99f53717156d598a4980746079d7b89a957d36
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2025-08-14 09:45:22 +0600

    first command line tool created

---
+++ CommandlineTools/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.25.0
+
+require syreclabs.com/go/faker v1.2.3 // indirect
---
+++ CommandlineTools/gololcat/fortune.py
@@ -0,0 +1,11 @@
+# fortune.py
+import random
+
+quotes = [
+    "A journey of a thousand miles begins with a single step.",
+    "Be yourself; everyone else is already taken.",
+    "Do or do not. There is no try.",
+    "The early bird catches the worm."
+]
+
+print(random.choice(quotes))
